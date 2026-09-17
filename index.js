4adf4ba72998f5717b94987767bab7ab28fd4640
farida-shawky <faridashawky@gmail.com>
2026-09-17 19:31:47 +0300

    update

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var numbtns = document.querySelectorAll("div button");
+var numbtns = document.querySelectorAll(".numbtn");
 var _display = document.querySelector(".display");
 var _delbtn = document.querySelector(".delbtn");
 
 numbtns.forEach((button) => {
   button.addEventListener("click", (e) => {
     var _input = e.target.innerText;
-    if (_input !== "del" && _input !== "C" && _input !== "=") {
-      _display.value += _input;
-    }
+
+    _display.value += _input;
   });
 });
 
